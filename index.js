619cb1b5c9e80337e5293b8d7db2422acacfb25b
SRJ <sarojkumardey.contact@gmail.com>
2024-04-08 18:45:30 +0530

    cors added

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const books = require("./routes/books.js");
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.use("/api", books);
 
