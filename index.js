5960a10f840e2ce9f18c77cc650d23334d45a340
SRJ <sarojkumardey.contact@gmail.com>
2024-04-25 11:29:30 +0530

    someting

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const books = require("./routes/books.js");
 const app = express();
 const PORT = 3000;
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "assets")));
 
 app.use("/api", books);
 
